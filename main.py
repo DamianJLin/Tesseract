4efaff7e4c5026582439b59e299039615c9c0e65
DamianJLin <31408307+damianjlin@users.noreply.github.com>
2019-12-27 17:11:08 +0100

    Added get_point() and get_point_input() static method to Game class to get the point input from players.

--- main.py
+++ main.py
@@ -64,6 +64,53 @@ class Game:
             # Print the gaps in w.
             print('\n\n', end='')
 
+    @staticmethod
+    def get_point(player):
+
+        def get_point_input():
+            """
+            Gets input from player and returns a point as a list of integers, or an error code.
+            :return: -1 if incorrect number of inputs
+                     -2 if inputs are not convertible to integers
+                     -3 if integers are outside of correct range
+                     otherwise list of inputs as integers
+            """
+
+            # Checks for the correct number of comma delimited inputs
+            coords_unparsed = input('Player {}\'s Turn. Enter your coordinates:'.format(player)).split(',')
+            if len(coords_unparsed) != 4:
+                return -1
+
+            # Checks that inputs are integers
+            try:
+                coords_unparsed = [int(i) for i in coords_unparsed]
+            except ValueError:
+                return -2
+
+            # Checks that integers are in the correct range
+            if not all(0 <= i < board_size for i in coords_unparsed):
+                return -3
+
+            return coords_unparsed
+
+        # Initialise point_input
+        point_input = None
+
+        # Keep trying inputs until player inputs correctly
+        while not isinstance(point_input, list):
+            point_input = get_point_input()
+            # Handling error codes.
+            if point_input == -1:
+                print('Incorrect number of comma-delimited inputs. Try again.')
+            if point_input == -2:
+                print('Comma-delimited inputs need to be integers. Try again.')
+            if point_input == -3:
+                print('Integers need to be in the range [0, {}]. Try again.'.format(board_size - 1))
+
+        assert isinstance(point_input, list)
+
+        return point_input
+
 
 if __name__ == '__main__':
     game = Game()
